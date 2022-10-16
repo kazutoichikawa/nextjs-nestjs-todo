10f06295835ae35f7e9892507b70027a72167a07
kazutoichikawa <74768712+kazutoichikawa@users.noreply.github.com>
2022-10-16 17:20:54 +0900

    prismaが発行したSQLをログに出力できるように改修

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,15 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
+
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit
+{
+  private readonly logger = new Logger(PrismaService.name);
   constructor(private readonly config: ConfigService) {
     super({
+      log: ['query', 'info', 'warn', 'error'],
       datasources: {
         db: {
           url: config.get('DATABASE_URL'),
         },
       },
     });
+  }
+
+  async onModuleInit() {
+    this.$on('query', (event) => {
+      this.logger.log(
+        `Query: ${event.query}`,
+        `Params: ${event.params}`,
+        `Duration: ${event.duration} ms`,
+      );
+    });
+    this.$on('info', (event) => {
+      this.logger.log(`message: ${event.message}`);
+    });
+    this.$on('error', (event) => {
+      this.logger.log(`error: ${event.message}`);
+    });
+    this.$on('warn', (event) => {
+      this.logger.log(`warn: ${event.message}`);
+    });
+    await this.$connect();
   }
 }
